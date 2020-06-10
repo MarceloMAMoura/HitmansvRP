4b6d55ecc61883831fdc8cd68f8253eb5ea64a4f
MarceloMAMoura <45248517+MarceloMAMoura@users.noreply.github.com>
2020-06-10 16:00:06 -0300

    Melhorias
    Adicionado valor minimo do contrato para R$100

--- Hitmans/hitman_c.lua
+++ Hitmans/hitman_c.lua
@@ -6,10 +6,10 @@ RegisterCommand("contrato", function(source, args)
 
 
 
- TriggerServerEvent('contrato', NetworkPlayerGetName(PlayerId()), args[1], args[2])
+TriggerServerEvent('contrato', NetworkPlayerGetName(PlayerId()), args[1], args[2])
 
-     -- we have to concatenate the table because the 'args' cb return a table (array)
-     -- the 2nd parameter in 'table.concat' is just spacing each args out
+-- we have to concatenate the table because the 'args' cb return a table (array)
+-- the 2nd parameter in 'table.concat' is just spacing each args out
 end)
 
 
@@ -18,10 +18,10 @@ RegisterCommand("pegarcontrato", function(source, args)
 
 
 
- TriggerServerEvent('pegarcontrato', NetworkPlayerGetName(PlayerId()), args[1])
+TriggerServerEvent('pegarcontrato', NetworkPlayerGetName(PlayerId()), args[1])
 
-     -- we have to concatenate the table because the 'args' cb return a table (array)
-     -- the 2nd parameter in 'table.concat' is just spacing each args out
+-- we have to concatenate the table because the 'args' cb return a table (array)
+-- the 2nd parameter in 'table.concat' is just spacing each args out
 end)
 
 
@@ -34,8 +34,8 @@ TriggerServerEvent('contratos', source)
 
 
 
-     -- we have to concatenate the table because the 'args' cb return a table (array)
-     -- the 2nd parameter in 'table.concat' is just spacing each args out
+-- we have to concatenate the table because the 'args' cb return a table (array)
+-- the 2nd parameter in 'table.concat' is just spacing each args out
 end)
 
 
@@ -44,40 +44,40 @@ end)
 
 RegisterNetEvent('showNotification')
 AddEventHandler('showNotification', function(text)
-	ShowNotification(text)
+ShowNotification(text)
 end)
 function ShowNotification(text)
-	SetNotificationTextEntry("STRING")
-	AddTextComponentString(text)
-	DrawNotification(0,1)
+  SetNotificationTextEntry("STRING")
+  AddTextComponentString(text)
+  DrawNotification(0,1)
 end
 Citizen.CreateThread(function()
-    -- main loop thing
-	alreadyDead = false
-    while true do
-        Citizen.Wait(50)
-		local playerPed = GetPlayerPed(-1)
-		if IsEntityDead(playerPed) and not alreadyDead then
-			killer = GetPedKiller(playerPed)
-			killername = false
-			for id = 0, 64 do
-				if killer == GetPlayerPed(id) then
-					killername = GetPlayerName(id)
-				end				
-			end
-			if killer == playerPed then
-				TriggerServerEvent('playerDied',0,0)
-			elseif killername then
-				TriggerServerEvent('playerDied',killername,1)
-			else
-				TriggerServerEvent('playerDied',0,2)
-			end
-			alreadyDead = true
-		end
-		if not IsEntityDead(playerPed) then
-			alreadyDead = false
-		end
-	end
+-- main loop thing
+alreadyDead = false
+while true do
+  Citizen.Wait(50)
+  local playerPed = GetPlayerPed(-1)
+  if IsEntityDead(playerPed) and not alreadyDead then
+    killer = GetPedKiller(playerPed)
+    killername = false
+    for id = 0, 64 do
+      if killer == GetPlayerPed(id) then
+        killername = GetPlayerName(id)
+      end
+    end
+    if killer == playerPed then
+      TriggerServerEvent('playerDied',0,0)
+    elseif killername then
+      TriggerServerEvent('playerDied',killername,1)
+    else
+      TriggerServerEvent('playerDied',0,2)
+    end
+    alreadyDead = true
+  end
+  if not IsEntityDead(playerPed) then
+    alreadyDead = false
+  end
+end
 end)
 
 
--- Hitmans/server.lua
+++ Hitmans/server.lua
@@ -16,39 +16,42 @@ AddEventHandler('contrato', function(player, nick, valor)--Recebe os valores do 
 local user_id = vRP.getUserId({source})
 local src = vRP.getUserSource({user_id})
 
-if nick ~= nil or valor ~= nil then
+local dinheiro = vRP.getMoney({user_id})
+dinheiro = tonumber(dinheiro)
+
+if nick ~= nil or valor ~= nil or dinheiro < 100 then
 
 
-MySQL.Async.execute("INSERT INTO hitman (player, valor_contrato) VALUES (@player, @valor_contrato)", {['@player'] = nick, ['@valor_contrato'] = valor})--Adiciona no banco de dados a vitima e o valor que está na cabeça dela
-
-
-
-	function vRP.giveMoney(user_id,valor)--retira dinheiro do jogador que usou o /contrato
-	local money = vRP.getMoney({user_id})
-	vRP.setMoney({user_id,money-valor})--O comando precisa ter ({valores}) para funcionar, o comando não irá funcionar de ficar .setMoney() sem os {}
-	end
-
-	function arredondar(num, numDecimal)
-	local mult = 5^(numDecimal or 0)
-	return math.floor(num * mult + 0.5) / mult
-	end
-
-
-	valor = arredondar(tonumber(valor),0)
-
-	local user_id = vRP.getUserId({source})
-	vRP.giveMoney(user_id,valor)
+  MySQL.Async.execute("INSERT INTO hitman (player, valor_contrato) VALUES (@player, @valor_contrato)", {['@player'] = nick, ['@valor_contrato'] = valor})--Adiciona no banco de dados a vitima e o valor que está na cabeça dela
 
 
 
-	TriggerClientEvent('chatMessage', -1, '^7[^1Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Algum jogador colocou contrato na cabeça do jogador '.. nick .. ' no valor de  R$'..valor) -- Mensagem sobre o contrato que foi colocado
+  function vRP.giveMoney(user_id,valor)--retira dinheiro do jogador que usou o /contrato
+    local money = vRP.getMoney({user_id})
+    vRP.setMoney({user_id,money-valor})--O comando precisa ter ({valores}) para funcionar, o comando não irá funcionar de ficar .setMoney() sem os {}
+  end
+
+  function arredondar(num, numDecimal)
+    local mult = 5^(numDecimal or 0)
+    return math.floor(num * mult + 0.5) / mult
+  end
+
+
+  valor = arredondar(tonumber(valor),0)
+
+  local user_id = vRP.getUserId({source})
+  vRP.giveMoney(user_id,valor)
+
+
+
+  TriggerClientEvent('chatMessage', -1, '^7[^1Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Algum jogador colocou contrato na cabeça do jogador '.. nick .. ' no valor de  R$'..valor) -- Mensagem sobre o contrato que foi colocado
 else
-vRPclient.notify(src,{"~w~[~r~Contrato~w~]~r~ ~g~Use o comando /contrato nick valor"})
---TriggerClientEvent('chatMessage', -1, '^7[^1Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Use o comando /contrato nick valor')
+  vRPclient.notify(src,{"~w~[~r~Contrato~w~]~r~ ~g~Use o comando /contrato nick valor, valor minimo R$100"})
+  --TriggerClientEvent('chatMessage', -1, '^7[^1Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Use o comando /contrato nick valor')
 end
 
 
-	
+
 end)
 
 
@@ -72,20 +75,20 @@ local src = vRP.getUserSource({user_id})
 if vitima ~= nil then
 
 
-local user_id = vRP.getUserId({source})
-if vRP.hasGroup({user_id,"Assassino lider"}) or vRP.hasGroup({user_id,"Assassino"}) then
+  local user_id = vRP.getUserId({source})
+  if vRP.hasGroup({user_id,"Assassino lider"}) or vRP.hasGroup({user_id,"Assassino"}) then
 
-MySQL.Async.fetchAll("UPDATE hitman SET assassino_resp=@assassino_resp, user_id_vrp=@user_id_vrp WHERE player=@player", {['@assassino_resp'] = player, ['@user_id_vrp'] = user_id, ['@player'] = vitima})
---atualiza no banco quem é o assassino/id vrp responsável pela vitima
+    MySQL.Async.fetchAll("UPDATE hitman SET assassino_resp=@assassino_resp, user_id_vrp=@user_id_vrp WHERE player=@player", {['@assassino_resp'] = player, ['@user_id_vrp'] = user_id, ['@player'] = vitima})
+    --atualiza no banco quem é o assassino/id vrp responsável pela vitima
 
---TriggerClientEvent('chatMessage', -1, '^7[^1Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], 'O jogador do RG '.. user_id .. ' de nome '..player.. ' pegou o contrado da vitima: '..vitima) -- Mensagem sobre o contrato que foi colocado
-end
+    --TriggerClientEvent('chatMessage', -1, '^7[^1Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], 'O jogador do RG '.. user_id .. ' de nome '..player.. ' pegou o contrado da vitima: '..vitima) -- Mensagem sobre o contrato que foi colocado
+  end
 else
-vRPclient.notify(src,{"~w~[~r~Contrato~w~]~r~ ~g~/pegarcontrato Nick_da_vitima"})
---TriggerClientEvent('chatMessage', -1, '^7[^Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], '/pegarcontrato Nick da vitima')
+  vRPclient.notify(src,{"~w~[~r~Contrato~w~]~r~ ~g~/pegarcontrato Nick_da_vitima"})
+  --TriggerClientEvent('chatMessage', -1, '^7[^Contrato^7]^2', {0,0,0} --[[ this table is just rgb ]], '/pegarcontrato Nick da vitima')
 end
 
-	
+
 end)
 
 
@@ -110,38 +113,38 @@ if vRP.hasGroup({user_id,"Assassino lider"}) or vRP.hasGroup({user_id,"Assassino
 
 
 
-MySQL.Async.fetchAll("SELECT COUNT(*) as count FROM hitman WHERE assassino_resp IS NULL",{}, 
-		function(contagemDB)
-			if tonumber(contagemDB[1].count) ~= 0 then
-					MySQL.Async.fetchAll("SELECT * FROM hitman WHERE assassino_resp IS NULL",{}, 
-					function(result)
-						vRPclient.notify(src,{'~w~[~r~Hitman~w~]~r~ ~y~Contrato na vitima: '..result[1].player..' no valor de R$'..result[1].valor_contrato})
-						--TriggerClientEvent('chatMessage', -1, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Contrato na vitima: '..result[1].player..' no valor de R$'..result[1].valor_contrato) -- Comando mostra no char o contrato disponivel
+  MySQL.Async.fetchAll("SELECT COUNT(*) as count FROM hitman WHERE assassino_resp IS NULL",{},
+  function(contagemDB)
+    if tonumber(contagemDB[1].count) ~= 0 then
+      MySQL.Async.fetchAll("SELECT * FROM hitman WHERE assassino_resp IS NULL",{},
+      function(result)
+        vRPclient.notify(src,{'~w~[~r~Hitman~w~]~r~ ~y~Contrato na vitima: '..result[1].player..' no valor de R$'..result[1].valor_contrato})
+        --TriggerClientEvent('chatMessage', -1, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Contrato na vitima: '..result[1].player..' no valor de R$'..result[1].valor_contrato) -- Comando mostra no char o contrato disponivel
 
-					end)
-			else
-			vRPclient.notify(src,{"~w~[~r~Hitman~w~]~r~ ~g~Sem contratos no momento"})
-	--		TriggerClientEvent('showNotification', player, "~w~[~r~Hitman~w~]~r~ ~g~Sem contratos no momento"..player) 
-			--TriggerClientEvent('chatMessage', player, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Sem contratos no momento')
-			end
-		end)
-	end
-	
-	
-	
-end)
+        end)
+      else
+        vRPclient.notify(src,{"~w~[~r~Hitman~w~]~r~ ~g~Sem contratos no momento"})
+        --		TriggerClientEvent('showNotification', player, "~w~[~r~Hitman~w~]~r~ ~g~Sem contratos no momento"..player)
+        --TriggerClientEvent('chatMessage', player, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Sem contratos no momento')
+      end
+      end)
+    end
 
 
------------------------------------------Fim do comando /contratos--------------------------------------------------
+
+    end)
 
 
+    -----------------------------------------Fim do comando /contratos--------------------------------------------------
 
 
 
 
 
 
------------------------------------------Recompensa ao matar a vitima----------------------------------------------
+
+
+    -----------------------------------------Recompensa ao matar a vitima----------------------------------------------
 
 
 
@@ -149,77 +152,74 @@ end)
 
 RegisterServerEvent('playerDied')
 AddEventHandler('playerDied',function(killer,reason)
-	if killer == "**Invalid**" then --Can't figure out what's generating invalid, it's late. If you figure it out, let me know. I just handle it as a string for now.
-		reason = 2
-	end
-	if reason == 0 then
-		TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ committed suicide. ")
-	elseif reason == 1 then
-		TriggerClientEvent('showNotification', -1,"~b~".. killer .. "~w~ matou ~r~"..GetPlayerName(source).."~w~.")
-		
-		local assassino = killer
-		local vitima = GetPlayerName(source)
-		
-		
+if killer == "**Invalid**" then --Can't figure out what's generating invalid, it's late. If you figure it out, let me know. I just handle it as a string for now.
+      reason = 2
+    end
+    if reason == 0 then
+      TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ committed suicide. ")
+    elseif reason == 1 then
+      TriggerClientEvent('showNotification', -1,"~b~".. killer .. "~w~ matou ~r~"..GetPlayerName(source).."~w~.")
 
-		
-		MySQL.Async.fetchAll("SELECT COUNT(*) as count FROM hitman WHERE assassino_resp IS NOT NULL",{}, 
-		function(contagemDB)
-			if tonumber(contagemDB[1].count) ~= 0 then
+      local assassino = killer
+      local vitima = GetPlayerName(source)
 
-		
-		MySQL.Async.fetchAll("SELECT * FROM hitman WHERE player=@player",{['@player'] = vitima}, 
-		function(result)
-		    if result[1].player == vitima and result[1].assassino_resp == killer then--Se a vitima for uma pessoa do banco de dados E o assassino for o responsável pelo /pegarcontrato for o killer, ele continua a condição
-		
-		
-					function vRP.giveMoney(user_id,valor)--adiciona dinheiro caso o assassino que usou o comando /pegarcontrato nick mate a vitima
-					local money = vRP.getMoney({user_id})
-					vRP.setMoney({user_id,money+valor})--O comando precisa ter ({valores}) para funcionar, o comando não irá funcionar de ficar .setMoney() sem os {}
-					end
 
-					function arredondar(num, numDecimal)
-					local mult = 5^(numDecimal or 0)
-					return math.floor(num * mult + 0.5) / mult
-					end
 
-					local valor = result[1].valor_contrato
-					local user_id = result[1].user_id_vrp
-			
-			
-					if vRP.hasGroup({user_id,"Assassino lider"}) or vRP.hasGroup({user_id,"Assassino"}) then
-					valor = arredondar(tonumber(valor),0)
-					vRP.giveMoney(user_id,valor)
-			
 
-					TriggerClientEvent('chatMessage', -1, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Algum assassino cumpriu o contrato na cabeça de '..result[1].player..' no valor de R$'..result[1].valor_contrato)
-					--TriggerClientEvent('chatMessage', -1, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'RG: '..result[1].user_id_vrp..' O assassino '.. result[1].assassino_resp .. ' cumpriu o contrato na cabeça de '..result[1].player.. ' e ganhou '..result[1].valor_contrato)
-					--O comando acima manda msg para o servidor informando os dados do assassino que matou a vitima
-		
-		
-					--Ao cumprir o contrato, deleta as informações do alvo do banco de dados
-					MySQL.Async.fetchAll("DELETE FROM hitman WHERE player=@player", {['@player'] = vitima})
-					--assassino_resp = nick
-					--user_id_vrp = id vrp do assassino
-					vitima = nil
-					assassino = nil
-					return
-			
-			
-					end
-				end
-			end)
-		end
-	end)
-	
-	else
-		TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ died.")
-	end
-	
-	
-	
-	
-end)
+      MySQL.Async.fetchAll("SELECT COUNT(*) as count FROM hitman WHERE assassino_resp IS NOT NULL",{},
+      function(contagemDB)
+        if tonumber(contagemDB[1].count) ~= 0 then
+
+
+          MySQL.Async.fetchAll("SELECT * FROM hitman WHERE player=@player",{['@player'] = vitima},
+          function(result)
+            if result[1].player == vitima and result[1].assassino_resp == killer then--Se a vitima for uma pessoa do banco de dados E o assassino for o responsável pelo /pegarcontrato for o killer, ele continua a condição
+
+
+              function vRP.giveMoney(user_id,valor)--adiciona dinheiro caso o assassino que usou o comando /pegarcontrato nick mate a vitima
+                local money = vRP.getMoney({user_id})
+                vRP.setMoney({user_id,money+valor})--O comando precisa ter ({valores}) para funcionar, o comando não irá funcionar de ficar .setMoney() sem os {}
+              end
+
+              function arredondar(num, numDecimal)
+                local mult = 5^(numDecimal or 0)
+                return math.floor(num * mult + 0.5) / mult
+              end
+
+              local valor = result[1].valor_contrato
+              local user_id = result[1].user_id_vrp
+
+
+              if vRP.hasGroup({user_id,"Assassino lider"}) or vRP.hasGroup({user_id,"Assassino"}) then
+                valor = arredondar(tonumber(valor),0)
+                vRP.giveMoney(user_id,valor)
+
+
+                TriggerClientEvent('chatMessage', -1, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'Algum assassino cumpriu o contrato na cabeça de '..result[1].player..' no valor de R$'..result[1].valor_contrato)
+                --TriggerClientEvent('chatMessage', -1, '^7[^1Hitman^7]^2', {0,0,0} --[[ this table is just rgb ]], 'RG: '..result[1].user_id_vrp..' O assassino '.. result[1].assassino_resp .. ' cumpriu o contrato na cabeça de '..result[1].player.. ' e ganhou '..result[1].valor_contrato)
+                --O comando acima manda msg para o servidor informando os dados do assassino que matou a vitima
+
+
+                --Ao cumprir o contrato, deleta as informações do alvo do banco de dados
+                MySQL.Async.fetchAll("DELETE FROM hitman WHERE player=@player", {['@player'] = vitima})
+                --assassino_resp = nick
+                --user_id_vrp = id vrp do assassino
+                vitima = nil
+                assassino = nil
+                return
+
+
+              end
+            end
+            end)
+          end
+          end)
+
+        else
+          TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ died.")
+        end
+
+    end)
 
 
 
